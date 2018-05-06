74fa7eed342716477a665a5787fcbfb825a34a2d
Unknown <the-deponian@gmx.de>
2018-05-06 14:55:09 +0200

    kabooms

---
+++ Assets/BombExplosion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BombExplosion : MonoBehaviour {
+
+	public ParticleSystem explosionParticles;
+	public GameObject graphics;
+	public Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+	void OnCollisionEnter2D (Collision2D coll) {
+	
+		Destroy(gameObject, 2);
+		rigid.simulated = false;
+		graphics.SetActive(false);
+
+		GameObject explodoWalls = GameObject.FindWithTag("ExplodoWalls");
+		Tilemap wallsExplodo = explodoWalls.GetComponent<Tilemap>();
+		Vector3Int gridPosBomb = wallsExplodo.WorldToCell(transform.position);
+		
+		for (int x = gridPosBomb.x-5 ; x<=gridPosBomb.x+5 ; x++) {
+			for (int y = gridPosBomb.y-5 ; y<=gridPosBomb.y+5 ; y++) {
+				Vector3Int explodingTile = new Vector3Int (x,y,gridPosBomb.z);
+				Vector3 worldTilePos = wallsExplodo.CellToWorld(explodingTile);
+				float distance = Vector2.Distance(transform.position, worldTilePos);
+				if (distance < 10) {
+					wallsExplodo.SetTile(explodingTile, null);
+				}
+			}
+		}
+		explosionParticles.Play();
+	}
+}
---
+++ Assets/BombThrow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BombThrow : MonoBehaviour {
+
+	public Transform startPoint ;
+	public Vector2 throwDirection ;
+	public Rigidbody2D bomb ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButtonDown ("Bomb")) {
+			Rigidbody2D newBomb = Instantiate(bomb, startPoint.position, Quaternion.identity);
+			Vector2 worldThrow = startPoint.TransformVector(throwDirection);
+			newBomb.AddForce(worldThrow, ForceMode2D.Impulse);
+		}
+
+	}
+}
